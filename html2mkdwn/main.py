aa4515cf13abcb08408da72c614b9bd15424dd96
Konstantin Auffinger <konstantinauffinger@gmail.com>
2024-03-16 18:21:58 +0100

    feat: fix issues

--- html2mkdwn/main.py
+++ html2mkdwn/main.py
@@ -1,36 +1,47 @@
+"""This is the main file for the FastAPI application. It contains the logic for extracting markdown from HTML content and URLs. The application uses the trafilatura library to extract markdown from HTML content. """
+
+import trafilatura
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-import trafilatura
 
 app = FastAPI()
 
 
 class TextExtractionRequest(BaseModel):
+    """Request model for extracting markdown from html content"""
+
     html_content: str
 
 
 @app.post("/html/")
 async def extract_markdown(request: TextExtractionRequest):
+    """Extract markdown from html content"""
     try:
         markdown = trafilatura.extract(
             filecontent=request.html_content, output_format="markdown"
         )
         if markdown:
             return {"markdown": markdown}
-        else:
-            raise HTTPException(
-                status_code=404, detail="Extraction failed: No content extracted"
-            )
+
+        raise HTTPException(
+            status_code=404, detail="Extraction failed: No content extracted"
+        )
+
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Extraction error: {str(e)}")
+        raise HTTPException(
+            status_code=500, detail=f"Extraction error: {str(e)}"
+        ) from e
 
 
 class URLExtractionRequest(BaseModel):
+    """Request model for extracting markdown from a URL"""
+
     url: str
 
 
 @app.post("/url/")
 async def extract_markdown_from_url(request: URLExtractionRequest):
+    """Extract markdown from a URL"""
     try:
         html_content = trafilatura.fetch_url(request.url)
         markdown = trafilatura.extract(
@@ -38,10 +49,13 @@ async def extract_markdown_from_url(request: URLExtractionRequest):
         )
         if markdown:
             return {"markdown": markdown}
-        else:
-            raise HTTPException(status_code=404, detail="Extraction failed")
+
+        raise HTTPException(status_code=404, detail="Extraction failed")
+
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Extraction error: {str(e)}")
+        raise HTTPException(
+            status_code=500, detail=f"Extraction error: {str(e)}"
+        ) from e
 
 
 if __name__ == "__main__":
